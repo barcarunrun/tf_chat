c1d29dd940da753f08e313adc70f331b5cd9c5f4
eiichi sugiyama <barcarunrunbiscabarca@gmail.com>
2021-07-14 10:58:13 +0900

    sic

--- libs/django/contrib/admin/templatetags/admin_static.py
+++ libs/django/contrib/admin/templatetags/admin_static.py
@@ -10,8 +10,8 @@ _static = None
 def static(path):
     global _static
     if _static is None:
-        if apps.is_installed('django.contrib.staticfiles'):
-            from django.contrib.staticfiles.templatetags.staticfiles import static as _static
-        else:
-            from django.templatetags.static import static as _static
+        # if apps.is_installed('django.contrib.staticfiles'):
+        #     from django.contrib.staticfiles.templatetags.staticfiles import static as _static
+        # else:
+        from django.templatetags.static import static as _static
     return _static(path)
